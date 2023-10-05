ad8c4f837bccc361d3b9531c39845e70445b59a1
Gaiser1017 <145823235+Gaiser1017@users.noreply.github.com>
2023-10-05 01:08:27 -0700

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,8 +9,9 @@ public class HomeController : Controller
             new StudentInfoModel { Name = "Kyla Gaiser", StudentId = "21-2205", StudentAge = "20" },
             new StudentInfoModel { Name = "Nj Ferrer", StudentId = "21-2206", StudentAge = "21" },
             new StudentInfoModel { Name = "Kurt Delos Santos", StudentId = "21-2207", StudentAge = "22" },
-            new StudentInfoModel { Name = "Trisha", StudentId = "21-2208", StudentAge = "23" }
-            new StudentInfoModel { Name = "Aldrian Bersamina", StudentId = "21-2208", StudentAge = "24" }
+            new StudentInfoModel { Name = "Trisha", StudentId = "21-2208", StudentAge = "23" },
+            new StudentInfoModel { Name = "Aldrian Bersamina", StudentId = "21-2208", StudentAge = "24" },
+            new StudentInfoModel { Name = "Piolo Moneva", StudentId = "21-2209", StudentAge = "25" }
         };
 
         return View(studentInfoArray);
